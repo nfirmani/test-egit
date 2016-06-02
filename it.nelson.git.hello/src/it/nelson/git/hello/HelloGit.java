5ae08646ddaf5f774f8e4c9a9d1ed7147a51fc28
Nelson <Nelson@Nelson-PC>
2016-06-02 21:56:39 +0200

    primo cambiamento al sorgente

--- it.nelson.git.hello/src/it/nelson/git/hello/HelloGit.java
+++ it.nelson.git.hello/src/it/nelson/git/hello/HelloGit.java
@@ -3,7 +3,8 @@ package it.nelson.git.hello;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git!");
+		System.out.println("Hello git! ");
+		System.out.println("Primo cambiamento.");
 
 	}
 
